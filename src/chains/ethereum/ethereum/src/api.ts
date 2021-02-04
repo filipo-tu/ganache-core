57ea74bcd2410662f6bc8232c0147b647aed8a4a
eggplantzzz <tmfeickert@gmail.com>
2021-02-04 17:52:26 -0500

    Use the options to determine difficulty and not the previous block header

--- src/chains/ethereum/ethereum/src/api.ts
+++ src/chains/ethereum/ethereum/src/api.ts
@@ -727,7 +727,7 @@ export default class EthereumApi implements types.Api {
         parentHeader.miner,
         tx.gas,
         parentHeader.timestamp,
-        parentHeader.difficulty
+        options.miner.difficulty
       );
       const runArgs = {
         tx: tx,
@@ -1778,7 +1778,7 @@ export default class EthereumApi implements types.Api {
       blockchain.coinbase,
       gas.toBuffer(),
       parentHeader.timestamp,
-      parentHeader.difficulty
+      options.miner.difficulty
     );
 
     const simulatedTransaction = {
