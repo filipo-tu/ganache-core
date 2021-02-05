3a61497219a09a11e82cfa7bab086fa32ef1baa9
Mike Seese <seesemichaelj@gmail.com>
2021-02-05 14:26:39 -0800

    fix miner ids to start at 1000; accounts start at 100

--- src/chains/filecoin/filecoin/src/things/address.ts
+++ src/chains/filecoin/filecoin/src/things/address.ts
@@ -43,6 +43,7 @@ class Address extends SerializableLiteral<AddressConfig> {
   }
 
   static readonly FirstNonSingletonActorId = 100; // Ref impl: https://git.io/JtgqT
+  static readonly FirstMinerId = 1000; // Ref impl: https://git.io/Jt2WE
   static readonly CHECKSUM_BYTES = 4;
 
   #privateKey?: string;
@@ -280,6 +281,7 @@ class Address extends SerializableLiteral<AddressConfig> {
   static fromId(
     id: number,
     isSingletonSystemActor: boolean = false,
+    isMiner: boolean = false,
     network: AddressNetwork = AddressNetwork.Testnet
   ): Address {
     if (Math.round(id) !== id || id < 0) {
@@ -288,7 +290,11 @@ class Address extends SerializableLiteral<AddressConfig> {
 
     return new Address(
       `${network}${AddressProtocol.ID}${
-        isSingletonSystemActor ? id : Address.FirstNonSingletonActorId + id
+        isSingletonSystemActor
+          ? id
+          : isMiner
+          ? Address.FirstMinerId + id
+          : Address.FirstNonSingletonActorId + id
       }`
     );
   }
